ec45a4c76dc30b3981f554b0abeb2cfc9016ec56
Rohan Serfontein <mossonthetree@Rohans-MBP.home>
2020-11-22 21:03:40 +0100

    setup as xcode project

--- FileShare/FileShare/SockAddress.h
+++ FileShare/FileShare/SockAddress.h
@@ -3,6 +3,8 @@
 #ifdef _WIN32
 #include <WinSock2.h>
 #include <ws2tcpip.h>
+#else
+#include <arpa/inet.h>
 #endif
 
 #include <string>
---
+++ FileShare/FileShare/main.cpp
@@ -0,0 +1,10 @@
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
