291782f06e688acbe040531061411d75aaa37caa
Rohan Serfontein <mossonthetree@Rohans-MBP.home>
2020-11-25 19:20:09 +0100

    updates for mac

--- FileShare/FileShare/main.cpp
+++ FileShare/FileShare/main.cpp
@@ -55,22 +55,24 @@ int main(int argc, const char * argv[])
     SockAddress hostAddr("192.168.1.176", "Mac", 8081);
     UdpSocket socket(hostAddr);
     SockAddress remoteAddr("192.168.1.100", "Windows", 8081);
+    std::cout << "Waiting\n";
     std::string input;
     while (isRunning)
     {
         std::cin >> input;
         CommandInfo info;
         char inputData[1024];
+        info.type = CMD_CONNECTED;
+        if (input == "-quit")
+        {
+            std::cin >> input;
+            info.type = CMD_DISCONNETED;
+            isRunning = false;
+        }
         strcpy(inputData, input.c_str());
         inputData[input.length()] = '\0';
         info.data = reinterpret_cast<void *>(inputData);
         info.dataLength = strlen(inputData);
-        info.type = CMD_CONNECTED;
-        if (input == "-quit")
-        {
-            isRunning = false;
-            info.type = CMD_DISCONNETED;
-        }
         OutputMemoryStream stream;
         info.write(stream);
         socket.sendTo(stream.getBufferPtr(), stream.getLength(), remoteAddr);
